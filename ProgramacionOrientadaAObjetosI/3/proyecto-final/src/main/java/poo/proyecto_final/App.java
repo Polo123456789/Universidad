cadf00ba3a259ec5e6cef0aed2b21917a908530e
polo123456789 <pabsan02@gmail.com>
2021-09-04 12:14:14 -0600

    (+)<POOI/4> Base para el proyecto final

---
+++ ProgramacionOrientadaAObjetosI/3/proyecto-final/src/main/java/poo/proyecto_final/App.java
@@ -0,0 +1,11 @@
+package poo.proyecto_final;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ ProgramacionOrientadaAObjetosI/3/proyecto-final/src/test/java/poo/proyecto_final/AppTest.java
@@ -0,0 +1,20 @@
+package poo.proyecto_final;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+        assertEquals(1, 1);
+    }
+}
