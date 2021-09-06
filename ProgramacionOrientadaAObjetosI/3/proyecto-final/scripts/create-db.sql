70106b91aeae3408600d59ded3a2a99f2b5e3a32
polo123456789 <pabsan02@gmail.com>
2021-09-06 15:54:59 -0600

    (+)<POOI/3> Diseño de la DB y script para crearla

---
+++ ProgramacionOrientadaAObjetosI/3/proyecto-final/scripts/create-db.sql
@@ -0,0 +1,63 @@
+CREATE TABLE decano (
+    id integer PRIMARY KEY,
+    nombre text,
+    apellidos text
+);
+
+CREATE TABLE profesor (
+    id integer PRIMARY KEY,
+    nombre text,
+    apellidos text
+);
+
+CREATE TABLE horario (
+    id integer PRIMARY KEY,
+    aula integer NOT NULL,
+    dia integer NOT NULL,
+    inicio integer NOT NULL,
+    final integer NOT NULL
+);
+
+CREATE TABLE escuelaAcademica (
+    id integer PRIMARY KEY,
+    nombre text
+);
+
+CREATE TABLE curso (
+    id integer PRIMARY KEY,
+    nombre text,
+    idHorario integer NOT NULL,
+    idProfesor integer NOT NULL,
+
+    FOREIGN KEY(idHorario) REFERENCES horario(id),
+    FOREIGN KEY(idProfesor) REFERENCES profesor(id)
+);
+
+CREATE TABLE carrera (
+    id integer PRIMARY KEY,
+    nombre text,
+    idDecano integer NOT NULL,
+    idEscuela integer NOT NULL,
+
+    FOREIGN KEY(idDecano) REFERENCES decano(id),
+    FOREIGN KEY(idEscuela) REFERENCES escuelaAcademica(id)
+);
+
+CREATE TABLE director (
+    id integer PRIMARY KEY,
+    nombre text,
+    apellidos text,
+    idCarrera integer NOT NULL,
+
+    FOREIGN KEY(idCarrera) REFERENCES carrera(id)
+);
+
+CREATE TABLE carreraTieneCurso (
+    idCarrera integer NOT NULL,
+    idCurso integer NOT NULL,
+
+    FOREIGN KEY(idCarrera) REFERENCES carrera(id),
+    FOREIGN KEY(idCurso) REFERENCES curso(id),
+
+    PRIMARY KEY(idCarrera, idCurso)
+);
