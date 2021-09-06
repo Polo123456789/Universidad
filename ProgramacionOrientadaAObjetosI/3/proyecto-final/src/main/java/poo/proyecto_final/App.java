393bd97657f66e8fb5c71c49b48f9376be78a30b
polo123456789 <pabsan02@gmail.com>
2021-09-06 12:33:08 -0600

    (+)<POOI/3> Funciona la base de datos

--- ProgramacionOrientadaAObjetosI/3/proyecto-final/src/main/java/poo/proyecto_final/App.java
+++ ProgramacionOrientadaAObjetosI/3/proyecto-final/src/main/java/poo/proyecto_final/App.java
@@ -1,11 +1,40 @@
 package poo.proyecto_final;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
- * Hello world!
- *
+ * Primer test para concectarse a una base de datos
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // Nos aseguramos de tener el driver
+        try {
+            Class.forName("org.sqlite.JDBC");
+        } catch (final ClassNotFoundException e) {
+            System.err.println(e);
+            return;
+        }
+
+        Connection con = null;
+
+        try {
+            String connection_string = "jdbc:sqlite:test.db";
+
+            con = DriverManager.getConnection(connection_string);
+
+            System.out.println("Se ha establecido la concecion a la base de datos");
+        } catch (final SQLException e) {
+            System.err.println(e.getMessage());
+        } finally {
+            try {
+                if (con != null) {
+                    con.close();
+                }
+            } catch (final SQLException e) {
+                System.err.println(e.getMessage());
+            }
+        }
     }
 }
