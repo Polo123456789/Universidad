6eab630e277b32a423e92d4fb8c8a16a32743d67
polo123456789 <pabsan02@gmail.com>
2021-09-08 10:20:04 -0600

    (+)<POOI/3> Gestion de carreras

---
+++ ProgramacionOrientadaAObjetosI/3/proyecto-final/src/main/java/poo/proyecto_final/Carrera.java
@@ -0,0 +1,250 @@
+package poo.proyecto_final;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class Carrera {
+    // Para indicar que no se ha ingresado a la base de datos
+    protected final static Integer noId = -1;
+
+    private Integer id;
+    private String nombre;
+    private Integer idDecano;
+    private Integer idEscuela;
+
+    public static ArrayList<Carrera> cargarDesde(DB                     db,
+                                                 final EscuelaAcademica e)
+        throws SQLException {
+
+        String query = "SELECT * FROM carrera";
+
+        if (e != null) {
+            query += " WHERE idCarrera = " + e.getId();
+        }
+
+        ArrayList<Carrera> carreras =
+            new ArrayList<Carrera>();
+        ResultSet rs = 
+            db.ejecutarQuery(query).getResultSet();
+
+        while (rs.next()) {
+            Carrera c = new Carrera();
+            c.id = rs.getInt("id");
+            c.nombre = rs.getString("nombre");
+            c.idDecano = rs.getInt("idDecano");
+            c.idEscuela = rs.getInt("idEscuela");
+        }
+        return carreras;
+    }
+
+    public static Carrera leerDesdeTerminal(final EscuelaAcademica  e,
+                                            final ArrayList<Decano> decanos) {
+        Carrera c = new Carrera();
+
+        System.out.println("Ingrese los datos de la carrera:");
+
+        c.nombre = Input.leerNombre();
+
+        System.out.println("Los decanos que se pueden encargar de esta "
+                           + "carrera son:");
+        Decano.enlistarDecanos(decanos);
+
+        System.out.print("A cual de ellos se la quiere asignar? ");
+        final int seleccionado = Input.leerNumero(
+            new Input.Rango(1, decanos.size() + 1)
+        );
+
+        final Decano decano = decanos.get(seleccionado - 1);
+
+        c.idDecano = decano.getId();
+        c.idEscuela = e.getId();
+        return c;
+    }
+
+    public static void enlistarCarreras(ArrayList<Carrera> carreras) {
+        for (int i = 0; i < carreras.size(); i++) {
+            System.out.println((i + 1) + ") " + carreras.get(i).nombre);
+        }
+    }
+
+    public static void gestionar(DB db, EscuelaAcademica e) 
+        throws SQLException, InterruptedException {
+
+        TermUtil.limpiarPantalla();
+        System.out.println(
+            Colors.blue("Gestion de carreras de \"" + e.getNombre() + "\"")
+        );
+
+        TermUtil.imprimirOpcionesGestion();
+        System.out.print(Colors.blue("Opcion: "));
+        final int opcion = Input.leerNumero(
+                new Input.Rango(1, TermUtil.opcionesDeGestion.length + 1)
+        );
+
+        switch (opcion) {
+            case 1: 
+                gestionAgregar(db, e);
+                break;
+
+            case 2: 
+                gestionModificar(db, e);
+                break;
+
+            case 3:
+                gestionEliminar(db, e);
+                break;
+
+            case 4:
+                // Salir
+                return;
+        }
+    }
+
+    private static void gestionAgregar(DB db, final EscuelaAcademica e) 
+        throws SQLException, InterruptedException {
+
+        ArrayList<Decano> disponibles = Decano.decanosSinCarrera(db);
+        if (disponibles.size() == 0) {
+            System.out.println(
+                Colors.red("\nTiene que al menos un decano disponible para "
+                           + "gestionar la carrera antes de crearla.\n")
+            );
+            Thread.sleep(1000);
+            return;
+        }
+
+        Carrera c = leerDesdeTerminal(e, disponibles);
+        db.insertar(c);
+        System.out.println(Colors.green("Insertado correctamente"));
+        Thread.sleep(1000);
+    }
+
+    private static void gestionModificar(DB db, final EscuelaAcademica e) 
+        throws SQLException, InterruptedException {
+
+        ArrayList<Carrera> carreras = cargarDesde(db, e);
+
+        if (carreras.size() == 0) {
+            System.out.println(
+                Colors.red("\nTiene que crear al menos una carrera "
+                           + "antes de poder modificarlas.\n")
+            );
+            Thread.sleep(1000);
+            return;
+        }
+
+        enlistarCarreras(carreras);
+
+        System.out.print(Colors.blue("Que carrera quiere modificar? "));
+        final int aModificar = Input.leerNumero(
+            new Input.Rango(1, carreras.size())
+        );
+
+        System.out.println("Quiere modificar la carrera, o sus cursos? "
+                           + "(1: Carrera, 2: Cursos)");
+
+        
+        final int seleccion = Input.leerNumero(
+                new Input.Rango(1, 2)
+        );
+
+        if (seleccion == 1) {
+            Carrera c = carreras.get(aModificar - 1);
+            c.modificarDesdeTerminal(db);
+            db.actualizar(c);
+        } else {
+            // TODO Gestionar cursos
+        }
+
+        System.out.println(Colors.green("\nModificado exitosamente\n"));
+        Thread.sleep(1000);
+    }
+
+    private static void gestionEliminar(DB db, final EscuelaAcademica e)
+        throws SQLException, InterruptedException {
+
+        ArrayList<Carrera> carreras = cargarDesde(db, e);
+
+        if (carreras.size() == 0) {
+            System.out.println(
+                Colors.red("\nTiene que crear al menos una carrera "
+                           + "antes de poder eliminarlas.\n")
+            );
+            Thread.sleep(1000);
+            return;
+        }
+
+        enlistarCarreras(carreras);
+
+        System.out.print(Colors.blue("Que carrera quiere eliminar? "));
+        final int aModificar = Input.leerNumero(
+            new Input.Rango(1, carreras.size())
+        );
+
+        if (aModificar == 0) {
+            System.out.println(
+                Colors.red("\nCancelando ...\n")
+            );
+            Thread.sleep(1000);
+            return;
+        }
+
+        Carrera c = carreras.get(aModificar - 1);
+        // TODO Mostrar los hijos afectados
+        db.eliminar(c);
+
+        System.out.println(Colors.green("\nElimininado exitosamente\n"));
+        Thread.sleep(1000);
+    }
+
+    public void modificarDesdeTerminal(DB db) 
+        throws SQLException, InterruptedException {
+        System.out.println("Ingrese los datos nombres para los campos."
+                           + "Deje en blanco los campos de texto, o coloque "
+                           + "un 0 en los numericos si no desea cambiarlos");
+
+        String posibleNombre = Input.leerNombreOVacio();
+        if (posibleNombre != null) {
+            nombre = posibleNombre;
+        }
+
+        ArrayList<Decano> disponibles = Decano.decanosSinCarrera(db);
+        if (disponibles.size() == 0) {
+            System.out.println(
+                Colors.red("\nNo hay decanos disponiblesa asi que esta "
+                           + "carrera conservara el suyo\n")
+            );
+        } else {
+            System.out.println("Los decanos que se pueden encargar de esta "
+                               + "carrera son:");
+            Decano.enlistarDecanos(disponibles);
+
+            System.out.print("A cual de ellos se la quiere asignar? ");
+            final int seleccionado = Input.leerNumero(
+                    new Input.Rango(0, disponibles.size() + 1)
+                    );
+
+            if (seleccionado != 0) {
+                final Decano decano = disponibles.get(seleccionado - 1);
+                idDecano = decano.getId();
+            }
+        }
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public Integer getIdDecano() {
+        return idDecano;
+    }
+
+    public Integer getIdEscuela() {
+        return idEscuela;
+    }
+}
