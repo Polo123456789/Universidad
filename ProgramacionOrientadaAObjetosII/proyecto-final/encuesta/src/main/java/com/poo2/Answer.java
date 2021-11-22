a19fc13b192726d4788b4f647f3e9c3bf8cbc015
polo123456789 <pabsan02@gmail.com>
2021-11-22 13:48:46 -0600

    (+)<POOII/PF> Terminada parte 1 de la encuesta

---
+++ ProgramacionOrientadaAObjetosII/proyecto-final/encuesta/src/main/java/com/poo2/Answer.java
@@ -0,0 +1,60 @@
+package com.poo2;
+
+import java.util.Date;
+
+public class Answer {
+    public static enum Answers {
+        happy,
+        neutral,
+        sad
+    }
+
+    private final Integer questionId;
+    private final Answers answer;
+    private final Date date;
+
+    public Answer(Integer questionId, Answers answer) {
+        this.questionId = questionId;
+        this.answer = answer;
+        this.date = new Date();
+    }
+
+    public Answers getAnswer() {
+        return answer;
+    }
+
+    public Integer getQuestionId() {
+        return questionId;
+
+    }
+
+    @Override
+    public String toString() {
+        return "Answer [answer=" + answer + ", date=" + date
+               + ", questionId=" + questionId + "]";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Answer other = (Answer) obj;
+        if (answer != other.answer)
+            return false;
+        if (date == null) {
+            if (other.date != null)
+                return false;
+        } else if (!date.equals(other.date))
+            return false;
+        if (questionId == null) {
+            if (other.questionId != null)
+                return false;
+        } else if (!questionId.equals(other.questionId))
+            return false;
+        return true;
+    }
+}
--- ProgramacionOrientadaAObjetosII/proyecto-final/encuesta/src/main/java/com/poo2/PrimaryController.java
+++ ProgramacionOrientadaAObjetosII/proyecto-final/encuesta/src/main/java/com/poo2/PrimaryController.java
@@ -1,12 +1,92 @@
 package com.poo2;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.shape.Circle;
 
 public class PrimaryController {
+    @FXML
+    private Circle happyFaceCircle;
+    @FXML
+    private Circle neutralFaceCirle;
+    @FXML
+    private Circle sadFaceCircle;
+    @FXML
+    private Label questionLabel;
+
+    private Integer questionId = -1;
+    private List<Answer> answers = new ArrayList<>();
+
+    // Ill guess that the questions should be stored in the db, so they will be
+    // synchronized in between the apps, so this is just temporal for testing
+    //
+    // TODO: Remove
+    final private static String[] questions = {
+        "Nos recomendarias a tu amigos?",
+        "Como valorarias la atencion de nuestro personal?",
+        "Que opinas del tiempo de espera para ser atendido?",
+        "Como valorarias nuestra relacion calidad-precio?",
+        "En general, como valoras la lipmieza de la tienda?",
+    };
 
     @FXML
+    void initialize() {
+        // TODO: loadQuestions();
+        nextQuestion();
+    }
+
+    @FXML
+    void happyFaceCirleClick(MouseEvent event) throws IOException {
+        registerAnswer(Answer.Answers.happy);
+    }
+
+    @FXML
+    void neutralFaceCirleClick(MouseEvent event) throws IOException {
+        registerAnswer(Answer.Answers.neutral);
+    }
+
+    @FXML
+    void sadFaceCircleClick(MouseEvent event) throws IOException {
+        registerAnswer(Answer.Answers.sad);
+    }
+
+    private void registerAnswer(Answer.Answers ans) throws IOException {
+        Answer a = new Answer(questionId, ans);
+        answers.add(a);
+        if (allQuestionsWhereAnswered()) {
+            // TODO: saveAsnwers();
+
+            // This is just to check that the answers where saved correctly
+            // TODO: Remove
+            for (var i : answers) {
+                System.out.println(i);
+            }
+
+            switchToSecondary();
+            return;
+        } 
+        nextQuestion();
+    }
+
+    private void nextQuestion() {
+        questionId++;
+
+        // TODO: Update to use questions from db
+        questionLabel.setText(questions[questionId]);
+    }
+
+    private Boolean allQuestionsWhereAnswered() {
+        // TODO: Update to use questions from db
+        return questionId == questions.length - 1;
+    }
+
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
     }
+
 }
