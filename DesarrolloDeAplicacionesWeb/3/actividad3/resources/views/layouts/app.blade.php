a96db5220d38af6ada10254abff26e8bb361b82d
polo123456789 <pabsan02@gmail.com>
2022-05-09 16:49:42 -0600

    (+)<DAW/3> Actividad 3 "Lista"

--- DesarrolloDeAplicacionesWeb/3/actividad3/resources/views/layouts/app.blade.php
+++ DesarrolloDeAplicacionesWeb/3/actividad3/resources/views/layouts/app.blade.php
@@ -12,6 +12,7 @@
 
         <!-- Styles -->
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
 
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
@@ -19,13 +20,6 @@
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
             @include('layouts.navigation')
-
-            <!-- Page Heading -->
-            <header class="bg-white shadow">
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    {{ $header }}
-                </div>
-            </header>
 
             <!-- Page Content -->
             <main>
